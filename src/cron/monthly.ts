d523f695a111bede1f26f4eb76f9eec1d1a11490
David Sousa <davidsou.fer@gmail.com>
2024-11-21 15:45:41 -0300

    feat: config coverage report

--- src/cron/monthly.ts
+++ src/cron/monthly.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import SourceFacade from "../sources/source.facade";
 
 const monthlyTask = cron.schedule(
-  "0 2 * 1 *",
+  "0 2 * 1 *", // 2:00 AM on the first day of the month
   async () => {
     const terabyteSource = new SourceFacade();
     await terabyteSource.syncSecurityEvents();
--- src/cron/weekly.ts
+++ src/cron/weekly.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import SourceFacade from "../sources/source.facade";
 
 const weeklyTask = cron.schedule(
-  "30 1 * * 0",
+  "30 1 * * 0", // 1:30 AM on Sunday
   async () => {
     const terabyteSource = new SourceFacade();
     await terabyteSource.syncDeviceDetails();
