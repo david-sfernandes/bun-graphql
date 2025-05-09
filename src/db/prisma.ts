1f442589fd57118a1e14a924a5242f12bd6fd6ac
David Sousa <davidsou.fer@gmail.com>
2025-05-09 14:24:38 -0300

    temporally add bd url to Dockerfile

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from "../../prisma/generated/client"; // Adjust the path as necessary
+// import { PrismaClient } from "../../prisma/generated/client"; // Adjust the path as necessary
+
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
