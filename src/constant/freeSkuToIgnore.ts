6794b05ff4972932f63703525a283bd5f0d2caa7
David Sousa <davidsou.fer@gmail.com>
2025-05-19 12:11:38 -0300

    feat: added new free sku to ignore

--- src/constant/freeSkuToIgnore.ts
+++ src/constant/freeSkuToIgnore.ts
@@ -10,7 +10,10 @@ const FREE_SKU_TO_IGNORE = new Set([
   "1f2f344a-700d-42c9-9427-5cea1d5d7ba6",
   "6470687e-a428-4b7a-bef2-8a291ad947c9",
   "52ea0e27-ae73-4983-a08f-13561ebdb823",
-  "99049c9c-6011-4908-bf17-15f496e6519d"
+  "99049c9c-6011-4908-bf17-15f496e6519d",
+  "5b631642-bd26-49fe-bd20-1daaa972ef80",
+  "8f0c5670-4e56-4892-b06d-91c085d7004f",
+  "3f9f06f5-3c31-472c-985f-62d9c10ec167"
 ]);
 
 export default FREE_SKU_TO_IGNORE;
